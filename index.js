da3f720e0568c840e2dcc88ff920bea8f5f8276c
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-09 16:51:12 +0530

    added &copy

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path')
 
 app.get('/', (req, res) => {
-    console.log("==IP ADDR==")
     console.log(req.socket.remoteAddress)
     console.log(req.ip)
+    console.log("==IP ADDR==")
     console.log(req.headers['x-forwarded-for'])
     console.log("========USER AGENT=======")
-    console.warn(req.headers["user-agent"])
+    console.log(req.headers["user-agent"])
     res.sendFile(path.join(__dirname, 'temp.html'))
 });
 
