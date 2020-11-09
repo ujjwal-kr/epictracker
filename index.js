5628d1095385d29f4fe5a958ec0de0263f85798a
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-09 14:08:03 +0530

    added template

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { env } = require('process');
 const app = express();
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    console.log(req.socket.remoteAddress)
+    console.log(req.socket.localAddress)
+    console.log(req.socket.localPort)
+    console.log(req.headers['x-forwarded-for'])
+    res.sendFile(path.join(__dirname, 'temp.html'))
 });
 
-app.listen(3888, () => {
+app.listen(3888 || process.env.PORT, () => {
     console.log('Example app listening on port 3888!');
 });
