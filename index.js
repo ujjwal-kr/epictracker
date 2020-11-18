82eaada5277c1edd9d06d6c6f4cb6082d091913a
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-18 18:59:07 +0530

    scraped

--- index.js
+++ index.js
@@ -13,19 +13,27 @@ app.get('/', (req, res) => {
     console.log("========USER AGENT=======")
     console.log(req.headers["user-agent"])
 
+    const ip = req.headers['x-forwarded-for']
+    const agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+
+    const url = 'https://whatismyipaddress.com/ip/' + '223.238.100.186'
+
+    Axios.get(url, {headers: {'user-agent': agent}}).then(res => {
+        const $ = cheerio.load(res.data)
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(1) > td').text()) 
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(2) > td').text()) 
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(3) > td').text()) 
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(3) > td').text()) 
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(4) > td').text()) 
+        console.log($('#section_left_3rd > table > tbody > tr:nth-child(7) > td').text()) 
+    }).catch(e => {
+        console.log("Something Went Wrong, wonderfully handled exception", e)
+    })
+
     const data = {
         header: req.headers["user-agent"],
         ip: req.headers['x-forwarded-for'],
     }
-
-    const agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
-
-    const url = 'https://whatismyipaddress.com/ip/' + '223.238.100.186'
-    Axios.get(url, {headers: {'user-agent': agent}}).then(res => {
-        console.log(res)
-    }).catch(e => {
-        console.log("Something Went Wrong, wonderfully handled exception", e)
-    })
 
     res.render('./temp', {data})
 
@@ -34,3 +42,5 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log('Example app listening on port '+port);
 });
+
+// #section_left_3rd > table > tbody > tr:nth-child(7) > td
