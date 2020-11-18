4057ec71729264a881b07171415d66060a769064
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-18 17:24:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     console.log(req.socket.remoteAddress)
     console.log(req.ip)
@@ -9,9 +10,15 @@ app.get('/', (req, res) => {
     console.log(req.headers['x-forwarded-for'])
     console.log("========USER AGENT=======")
     console.log(req.headers["user-agent"])
-    res.sendFile(path.join(__dirname, 'temp.html'))
-});
 
-app.listen(process.env.PORT, () => {
-    console.log('PROGRAM LOADED INTO MEMORY');
+    const data = {
+        header: req.headers["user-agent"],
+        ip: req.headers['x-forwarded-for'],
+    }
+    res.render('./temp', {data})
+    // res.sendFile(path.join(__dirname, 'temp.html'))
+});
+const port = process.env.PORT | 3000;
+app.listen(port, () => {
+    console.log('Example app listening on port '+port);
 });
