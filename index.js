fd0bb010d4fcb72e73bbdd610df508d488c9c8d6
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-19 18:51:58 +0530

    dynamic ip

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.get('/', async (req, res) => {
     const ip = req.headers['x-forwarded-for']
     const agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
 
-    const url = "https://api.ip8.com/ip/lookup/223.238.102.248"
+    const url = "https://api.ip8.com/ip/lookup/"+ip
 
     await Axios.get(url, {
         headers: {
             'user-agent': agent,
         }
     }).then(result => {
-
+        console.log(JSON.stringify(result.data))
         const data = {
             headers: req.headers['user-agent'],
             ip,
