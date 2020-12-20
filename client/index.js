171b388607878ea4af8bd45ec2bed666f30fb728
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-20 17:52:45 +0530

    js err fixed

--- client/index.js
+++ client/index.js
@@ -1,3 +1,24 @@
+// Get and display the stuff
+
 const memory = navigator.deviceMemory;
+const platform = navigator.platform;
+
 const memeoryPhrase = `Your device memorory is around ${memory}`;
-let memeoryContainer = document.querySelector('.memory').textContent = memeoryPhrase;
+const platformPhrase = `You are running ${platform}`;
+
+document.querySelector('.memory').textContent = memeoryPhrase;
+document.querySelector('.platform').textContent = platformPhrase;
+
+// Request goes here
+
+nowFetch().then(response => {
+    console.log(response)
+})
+
+async function nowFetch() {
+   const response =  await fetch("https://mycookie-monster.herokuapp.com", {
+       headers: {'Content-Type': 'application/json'},
+       method: 'GET'
+   })
+   return response.json()
+}
