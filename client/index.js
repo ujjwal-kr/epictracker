b322cbde383fdab45600db6f920ebbdb78615362
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-12-21 00:28:29 +0530

    graphics info

--- client/index.js
+++ client/index.js
@@ -1,4 +1,3 @@
-// Get info 
 const memory = navigator.deviceMemory;
 const platform = navigator.platform;
 const hardwareConcurrency = navigator.hardwareConcurrency;
@@ -6,14 +5,33 @@ const language = navigator.language;
 // Phrases
 const memeoryPhrase = `Your device memorory [RAM] is around ${memory} GB`;
 const platformPhrase = `You are possibly running: ${platform}`;
-const hardwareConcurrencyPhrase = `You have ${hardwareConcurrency} logical processor cores`;
-const languagePhrase = `Your device language is ${language}`
+const hardwareConcurrencyPhrase = `You have ${hardwareConcurrency} logical processor cores running`;
+const languagePhrase = `Your device language is ${language}`;
 // Put info
 document.querySelector('.memory').textContent = memeoryPhrase;
 document.querySelector('.platform').textContent = platformPhrase;
 document.querySelector('.con').textContent = hardwareConcurrencyPhrase;
 document.querySelector('.lang').textContent = languagePhrase;
-
+// Try to get the graphichal processing unit info
+const canv = document.getElementById("canv");
+const gl = canv.getContext("experimental-webgl");
+const graphicsVendor = getGraphicalUnitExtensions(gl).vendor;
+const graphicsRenderer = getGraphicalUnitExtensions(gl).renderer;
+const graphicsInfoPhrase = `Your identfied graphics: ${graphicsRenderer}, VENDOR: ${graphicsVendor}`
+document.querySelector(".graphics").textContent = graphicsInfoPhrase;
+// Getting graphic extensions with WEBGL
+function getGraphicalUnitExtensions(gl){
+    const unMaskedInfo = {
+        renderer: '',
+        vendor: '',
+    }
+    const dbgRenderInfo = gl.getExtension("WEBGL_debug_renderer_info")
+    if (dbgRenderInfo != null) {
+        unMaskedInfo.renderer = gl.getParameter(dbgRenderInfo.UNMASKED_RENDERER_WEBGL);
+        unMaskedInfo.vendor = gl.getParameter(dbgRenderInfo.UNMASKED_VENDOR_WEBGL);
+    }
+    return unMaskedInfo;
+}
 // cookie string send
 async function bakeCookie(data) {
     const response = await fetch("http://localhost:5000/generate/"+ btoa(JSON.stringify(data)) , {
