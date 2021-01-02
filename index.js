9aa53cfc24e980254255496db69c02651a837f49
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-01-02 18:24:28 +0530

    weather hndler

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
     const ip = req.headers['x-forwarded-for']
     const agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
 
-    const url = "https://api.ip8.com/ip/lookup/"+ip;
+    const url = "https://api.ip8.com/ip/lookup/" + ip;
     // const url = "https://api.ip8.com/ip/lookup/176.10.112.40";
 
     await Axios.get(url, {
@@ -29,40 +29,49 @@ app.get('/', async (req, res) => {
             'user-agent': agent,
         }
     }).then(async result => {
-        await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${result.data.details.geoip[0].city.name}&appid=84f0c05e16abc57b03ca8fa00b59f78e&units=metric`).then((weather) => {
-            console.log(weather.data.weather[0].description)
-            console.log(weather.data.main.temp)
-            const data = {
-                headers: req.headers['user-agent'],
-                ip,
-                isp: result.data.details.geoip[0].isp.org,
-                ispFull: result.data.details.geoip[0].isp.ASNname,
-                city: result.data.details.geoip[0].city.name,
-                pin: result.data.details.geoip[0].city.zip,
-                timezone: result.data.details.geoip[0].city.timezone,
-                continent: result.data.details.geoip[0].continent.name,
-                country: result.data.details.geoip[0].country.name,
-                latitude: result.data.details.geoip[0].city.details.latitude,
-                longitude: result.data.details.geoip[0].city.details.longitude,
-                weather: weather.data.weather[0].description,
-                temperature: weather.data.main.temp
-            }
-    
-            return res.status(200).json(data)
-        }).catch(e => {
-            console.log( "Cant get weather data lol" ,e)
-        })
+        const data = {
+            headers: req.headers['user-agent'],
+            ip,
+            isp: result.data.details.geoip[0].isp.org,
+            ispFull: result.data.details.geoip[0].isp.ASNname,
+            city: result.data.details.geoip[0].city.name,
+            pin: result.data.details.geoip[0].city.zip,
+            timezone: result.data.details.geoip[0].city.timezone,
+            continent: result.data.details.geoip[0].continent.name,
+            country: result.data.details.geoip[0].country.name,
+            latitude: result.data.details.geoip[0].city.details.latitude,
+            longitude: result.data.details.geoip[0].city.details.longitude,
+        }
+
+        return res.status(200).json(data)
     }).catch(e => {
         console.log("Something Went Wrong, wonderfully handled exception", e)
     })
 
 });
 
+app.get('/weather/:city', async (req, res) => {
+    const city = req.params.city;
+    await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f0c05e16abc57b03ca8fa00b59f78e&units=metric`).then(weather => {
+        return res.json({
+            description: weather.data.weather[0].description,
+            temperature: weather.data.main.temp
+        })
+    }).catch(e => {
+        console.log("Cant get weather data lol", e)
+        return res.status(404).json({
+            message: "NF"
+        })
+    })
+})
 
-app.get('/generate/:data', async (req, res) => {    // Because URL PARAMS ARE COOLER
+
+app.get('/generate/:data', async (req, res) => { // Because URL PARAMS ARE COOLER
     // this can be stored in the Database now
     console.log(atob(req.params.data));
-    res.json({cookie: req.params.data})
+    res.json({
+        cookie: req.params.data
+    })
 })
 
 const port = process.env.PORT || 4000;
