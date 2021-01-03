ebbfb7255836033f9212ce3fc71599e93875a124
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-01-03 08:58:52 +0530

    vpn for desktop

--- client/index.js
+++ client/index.js
@@ -148,29 +148,31 @@ nowFetch().then(response => {
         document.querySelector('.temperature').textContent = temperaturePhrase;
     })
     // VPN DETECTION
-    const date = new Date(Date.now())
-    const vpnData = {timezone: data.timezone, deviceTime: date.getTime()}
-    let options = {
-        timeZone: vpnData.timezone,
-        hour: 'numeric',
-        minute: 'numeric',
-        month: 'numeric',
-        second: 'numeric',
-        year: 'numeric',
-        day: 'numeric'
-    };
-    formatter = new Intl.DateTimeFormat([], options);
-    const stringTime = formatter.format(new Date());
-    const IPtime = Date.parse(stringTime)
-    let diff = IPtime - Date.now()
-    if (diff < 0) {
-        diff = -1*diff
-    }
-    if(diff > 50000) {
-        console.log("YAY VPN")
-        document.querySelector('.VPN').textContent = `You ARE using a VPN`
-    }else {
-        console.log("NO VPN")
-        document.querySelector('.VPN').textContent = `You donot seem to Use a VPN`
+    if (deviceWidth > 990) {
+        const date = new Date(Date.now())
+        const vpnData = {timezone: data.timezone, deviceTime: date.getTime()}
+        let options = {
+            timeZone: vpnData.timezone,
+            hour: 'numeric',
+            minute: 'numeric',
+            month: 'numeric',
+            second: 'numeric',
+            year: 'numeric',
+            day: 'numeric'
+        };
+        formatter = new Intl.DateTimeFormat([], options);
+        const stringTime = formatter.format(new Date());
+        const IPtime = Date.parse(stringTime)
+        let diff = IPtime - Date.now()
+        if (diff < 0) {
+            diff = -1*diff
+        }
+        if(diff > 50000) {
+            console.log("YAY VPN")
+            document.querySelector('.VPN').textContent = `You ARE using a VPN`
+        }else {
+            console.log("NO VPN")
+            document.querySelector('.VPN').textContent = `You donot seem to Use a VPN`
+        }
     }
 })
